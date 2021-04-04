c71e7316202f287b9dda0a607d8b4e8c4f7bcc30
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2021-04-03 20:38:13 -0700

    Created admin page, changed client.js for testing current AJAX calls with creating users, and edited user_login.html with updated description for the input block.

--- client/js/client.js
+++ client/js/client.js
@@ -1,7 +1,7 @@
 
 function createNewUser() {
     const xhttp = new XMLHttpRequest();
-    let createuserurl = "https://seanwallace.ca/writeMysql/?name=";
+    let createuserurl = "https://seanwallace.ca/v1/?name=";
     let connect = "&password="
     let name = document.getElementById("userName").value;
     let password = document.getElementById("userPassword").value;
@@ -19,17 +19,20 @@ document.getElementById("createUser").onclick = createNewUser;
 
 function logIn() {
     const xhttp = new XMLHttpRequest();
-    let loginurl = "https://seanwallace.ca/writeMysql/?name=";
-    let connect = "&password="
+    let loginurl = "https://seanwallace.ca/v1/user?username=";
+    let connect = "&username="
     let name = document.getElementById("userName").value;
-    let password = document.getElementById("userPassword").value;
-    xhttp.open("GET", loginurl + name + connect + password, true);
-
+    // let password = document.getElementById("userPassword").value;
+    xhttp.open("POST", loginurl+name, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     xhttp.onreadystatechange = function () {
+        console.log(this.readyState);
+        console.log(this.status);
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("2ndEPUsage").innerHTML = this.responseText;
         }
+        document.getElementById("2ndEPUsage").innerHTML = this.responseText;
     };
 }
 
