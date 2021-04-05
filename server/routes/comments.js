74a4aa2134529e2fb481224944e18e196d77126b
Sean Wallace <swallace987@gmail.com>
2021-04-05 00:03:50 -0700

    added the backend for comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -5,4 +5,126 @@ const {incApi} = require("./admin");
 const express = require('express');
 const router = express.Router();
 
+router.post('/',(req,res)=>{
+    incApi("post/comments")
+    let {postID, content, userID} = req.body;
+
+    if(!postID||!content||!userID){
+        res.status(405).send("Invalid Input")
+    }else{
+        if(!Number.isInteger(user)||!Number.isInteger(postID)){
+            res.status(405).send("Invalid Input")
+        }else{
+            db.promise(`Select post_id FROM posts WHERE post_id = ${postID}`)
+            .then((result) => {
+
+                if(result[0]){
+                    db.promise(`Select user_id FROM users WHERE user_id = ${userID}`)
+                    .then((result) => {
+
+                        if(result[0]){
+                            db.promise(`INSERT INTO comments (content,post_id,user_id) VALUES ('${content}',${postID},${userID})`)
+                            .then((result) => {
+                                res.status(201).send("New comment created");
+                            }).catch((err) => {
+                                console.error(err);
+                                res.status(500).send("Internal database error");
+                            });
+                        }else{
+                            res.status(405).send("Invalid input")
+                        }
+                    }).catch((err) => {
+                        console.error(err);
+                        res.status(500).send("Internal database error");
+                    });
+                }else{
+                    res.status(405).send("Invalid input")
+                }
+
+                
+            }).catch((err) => {
+                console.log("bot")
+                console.error(err);
+                res.status(500).send("Internal database error");
+            });
+        }
+    }
+
+
+
+
+
+
+})
+router.put('/',(req,res)=>{
+    incApi("put/comments")
+    let {id, content} = req.body;
+
+    if(!id||!content||Number.isInteger(id)){
+        res.status(405).send("Invalid input")
+    }else{
+        db.promise(`Select comment_id FROM comments WHERE comment_id = ${id}`)
+        .then((result) => {
+            if(result[0]){
+                db.promise(`UPDATE comments SET content = '${content}' WHERE comment_id = ${id}`)
+                .then((result) => {
+                    res.status(200).send("Comment successfully updated");
+                }).catch((err) => {
+                    console.error(err);
+                    res.status(500).send("Internal database error");
+                });
+            }else{
+                res.status(404).send("Comment not found")
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).send("Internal database error");
+        });
+    }
+    
+})
+router.get('/commentsForPost/:postID',(req,res)=>{
+    let postID = Number.parseInt(req.params['postID'])
+    if(!postID||!Number.isInteger(postID)){
+        res.status(400).send("Invalid ID supplied");
+    }else{
+        db.query(`SELECT post_ID AS postID, comment_id AS id, content, user_id AS userID WHERE post_ID = ${postID}`)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.send(500).send("Internal Database error")
+        });
+    }
+})
+router.delete('/:commentID',(req,res)=>{
+    let commentID = req.params['commentID'];
+    commentID = Number.parseInt(commentID);
+    if(!commentID||!Number.isInteger(commentID)){
+        res.status(400).send("invalid ID supplied");
+    }else{
+        db.promise(`Select comment_id from comments where comment_id = ${commentID}`)
+        .then((result) => {
+            
+            if(result[0]){
+                db.promise(`DELETE FROM comments WHERE comment_id = ${commentID}`)
+                .then((result) => {
+                    res.status(200).send("Succesfully Deleted comment")
+                }).catch((err) => {
+                    console.error(err)
+                    res.status(500).send("Internal Database error")
+                });
+            }
+            else{
+                res.status(404).send("comment not found.")
+            }
+        }).catch((err) => {
+            console.log("bottom");
+            console.error(err)
+            res.status(500).send("Internal Database error")
+            
+        });
+    }
+
+})
+
 module.exports = router;
