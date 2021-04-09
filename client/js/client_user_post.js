fdb06d82120ca04fe22aa5f24c8d9d2f55e4317c
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2021-04-08 21:46:30 -0700

    Created the pages base on the wirefram without styling.

---
+++ client/js/client_user_post.js
@@ -0,0 +1,17 @@
+function loadPostPastEntry() {
+
+}
+
+function sumbitNewComment() {
+
+}
+
+function loadPost() {
+
+}
+
+function loadUser(){
+
+}
+document.getElementById("submit").onclick = sumbitNewComment;
+window.onload = loadUser,loadPost,loadPostPastEntry;
