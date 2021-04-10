f8159eb71720bcfd0b4035fcc253a173f06a3aef
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2021-04-09 22:06:14 -0700

    Added styling for all pages

---
+++ client/js/user_login.js
@@ -0,0 +1,12 @@
+function tocreateUser() {
+    window.location.href = "./new_user.html";
+   }
+document.getElementById("createUser").onclick = tocreateUser;
+
+function toHome() {
+    window.location.href = "./home.html";
+}
+document.getElementById("back").onclick = toHome;
+function logIn() {
+    const xhttp = new XMLHttpRequest();
+}
