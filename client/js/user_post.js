23098a911bef1debfd50d346ebb74307e9e965f4
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2021-04-10 22:13:36 -0700

    Updated so it loads specific post

--- client/js/user_post.js
+++ client/js/user_post.js
@@ -4,10 +4,43 @@ if(!token){
 }
 let testid = 2;
 const root = "https://seanwallace.ca/v1/";
+const params = new URLSearchParams(window.location.search);
 function loadCurrentPost() {
+    var payload = JSON.parse(window.atob(token.split('.')[1])); 
+    console.log(payload["user"]["username"]);
+    console.log(payload["user"]);
+    document.getElementById("userName").innerText = "Logged in as " + payload["user"]["username"];
+
     const xhttp = new XMLHttpRequest();
+    console.log(token);
     let loadPostPastEntryUrl = root + "posts";
-    xhttp.open()
+    xhttp.open("GET", loadPostPastEntryUrl);
+    xhttp.setRequestHeader("Authorization", "Bearer " +token);
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.send(null);
+
+    let currentPostUrl = window.location.href;
+    console.log(currentPostUrl);
+    console.log(params.get("id"));
+    let currentPostId = parseInt(params.get("id"));
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           var entryCollection = JSON.parse(this.responseText);
+           console.log(entryCollection);
+           let entryContainer = document.createElement("div");
+        entryContainer.id = "allEntry";
+        for (let i = 0; i < entryCollection.length; i++) {
+            if (entryCollection[i]["id"] === currentPostId) {      
+                console.log("exexuted");          
+                document.getElementById("postAuthor").innerText = entryCollection[i]["user"]["username"];
+                document.getElementById("postTitle").innerText = entryCollection[i]["title"];
+                document.getElementById("postContent").innerText = entryCollection[i]["content"];
+                }
+            }
+        }
+        
+    };
+   
 }
 
 function sumbitNewComment() {
@@ -16,6 +49,7 @@ function sumbitNewComment() {
 
 function loadPost() {
     const xhttp = new XMLHttpRequest();
+
 }
 
 function loadUser(){
@@ -29,7 +63,7 @@ function deletePost() {
     }
 }
 function deleteComment() {
-
+    const xhttp = new XMLHttpRequest();
 }
 function editComment() {
 
@@ -41,5 +75,8 @@ function deletePost() {
 function editPost() {
 
 }
-document.getElementById("submit").onclick = sumbitNewComment;
-window.onload = loadUser,loadPost,loadPostPastEntry;
+function load() {
+    loadCurrentPost();
+}
+// document.getElementById("submit").onclick = sumbitNewComment;
+window.onload = load;
